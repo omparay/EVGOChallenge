d5210be5e7abea46e727adf1313295d39a54675d
Oliver Paray <omparay@gmail.com>
2020-03-29 03:02:04 -0700

    Initial project structure and SwiftPM links to Apollo and SwiftRX frameworks.

---
+++ SpaceX Missions/SpaceX MissionsTests/SpaceX_MissionsTests.swift
@@ -0,0 +1,34 @@
+//
+//  SpaceX_MissionsTests.swift
+//  SpaceX MissionsTests
+//
+//  Created by Oliver Paray on 3/29/20.
+//  Copyright © 2020 Oliver Paray. All rights reserved.
+//
+
+import XCTest
+@testable import SpaceX_Missions
+
+class SpaceX_MissionsTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
