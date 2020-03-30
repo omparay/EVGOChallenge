d3b3020c0bfa7f68953e4f4dd576aaad24e7d60a
Oliver Paray <omparay@gmail.com>
2020-03-29 21:37:34 -0700

    Deserialization to json dictionary works but is slow. Will try encodable objects next.

--- SpaceX Missions/SpaceX Missions/MainViewController.swift
+++ SpaceX Missions/SpaceX Missions/MainViewController.swift
@@ -20,6 +20,14 @@ class MainViewController: UIViewController {
             switch result {
                 case .success(let data):
                 debugPrint("Data: \(data)")
+                guard let results = data.data,
+                    let jsonData = try? JSONSerialization.data(withJSONObject: results.jsonObject, options: .prettyPrinted),
+                    let jsonDict = try? JSONSerialization.jsonObject(with: jsonData, options: .mutableContainers) as? [String: Any],
+                    let launches = jsonDict["launchesPast"] as? [[String:String]]
+                else { return }
+                for item in launches {
+                    debugPrint("Launch: \(String(describing: item["mission_name"])) Date: \(String(describing: item["launch_date_utc"]))")
+                }
                 case .failure(let error):
                 debugPrint("Error: \(error)")
             }
